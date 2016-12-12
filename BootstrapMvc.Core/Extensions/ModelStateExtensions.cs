3dfa09e0cf9b4a4d6ef1417f76f19ae08bbe56d9
iframeworx <geert@iframeworx.be>
2016-12-12 16:38:59 +0100

    add modelstate extension and asp-for tag helper

---
+++ BootstrapMvc.Core/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BootstrapMvc.Core.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        public static string ToErrorMessage(this ModelStateDictionary modelState)
+        {
+            return string.Join("\n", modelState.Values.SelectMany(m => m.Errors).Select(e => e.ErrorMessage));
+        }
+
+        public static IEnumerable<string> ToErrorMessages(this ModelStateDictionary modelState)
+        {
+            return modelState.Values.SelectMany(m => m.Errors).Select(e => e.ErrorMessage);
+        }
+    }
+}
---
+++ BootstrapMvc.Core/Helpers/DisplayForTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BootstrapMvc.Core.Helpers
+{
+    [HtmlTargetElement("dt", Attributes = ForAttributeName)]
+    public class DisplayForTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "asp-for";
+
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var text = For.ModelExplorer.Metadata.DisplayName ?? For.ModelExplorer.Metadata.PropertyName; // For.ModelExplorer.GetSimpleDisplayText();
+
+            output.Content.SetContent(text);
+        }
+    }
+}
