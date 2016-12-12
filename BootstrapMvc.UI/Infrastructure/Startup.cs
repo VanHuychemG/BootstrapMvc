2fe34384c39a8956d0de4958262c24eaf2fc76b9
iframeworx <geert@iframeworx.be>
2016-12-12 16:49:03 +0100

    add appsettings

--- BootstrapMvc.UI/Infrastructure/Startup.cs
+++ BootstrapMvc.UI/Infrastructure/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -8,9 +10,22 @@ namespace BootstrapMvc.UI.Infrastructure
 {
     public class Startup
     {
-        public Startup()
-        {
+        private IConfigurationRoot Configuration { get; }
 
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+            if (env.IsDevelopment())
+                builder.AddUserSecrets();
+
+            Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
