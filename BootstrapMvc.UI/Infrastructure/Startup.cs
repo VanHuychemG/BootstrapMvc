bc4017d4eaa3b417d34cfea73144d2f1e1af9503
iframeworx <geert@iframeworx.be>
2016-12-12 16:45:01 +0100

    add startup constructor (appsettings per environmen)

--- BootstrapMvc.UI/Infrastructure/Startup.cs
+++ BootstrapMvc.UI/Infrastructure/Startup.cs
@@ -8,6 +8,11 @@ namespace BootstrapMvc.UI.Infrastructure
 {
     public class Startup
     {
+        public Startup()
+        {
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
