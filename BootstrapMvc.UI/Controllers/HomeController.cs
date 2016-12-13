28c0a26af699d07761ee8fc2f9e4b0129f763f51
iframeworx <geert@iframeworx.be>
2016-12-13 17:36:29 +0100

    add sample page for alerts

--- BootstrapMvc.UI/Controllers/HomeController.cs
+++ BootstrapMvc.UI/Controllers/HomeController.cs
@@ -25,6 +25,25 @@ namespace BootstrapMvc.UI.Controllers
             return View();
         }
 
+        public IActionResult Alerts()
+        {
+            ViewData["Message"] = "Samples of alert messages.";
+
+            Information("Fading information message.");
+            Information("Dismissable information message.", true);
+
+            Warning("Fading warning message.");
+            Warning("Dismissable warning message.", null, true);
+
+            Danger("Fading danger message.");
+            Danger("Dismissable danger message", null, true);
+
+            Success("Fading success message.");
+            Success("Dismissable success message.", true);
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View();
--- BootstrapMvc.UI/wwwroot/js/site.js
+++ BootstrapMvc.UI/wwwroot/js/site.js
@@ -1,1 +1,51 @@
-﻿// Write your Javascript code.
+﻿$(document)
+    .ready(function () {
+
+        var passwordToggler = function (element, field) {
+            this.element = element;
+            this.field = field;
+
+            this.toggle();
+        };
+
+        passwordToggler.prototype = {
+            toggle: function () {
+                var self = this;
+                self.element.addEventListener("change", function () {
+                    if (self.element.checked) {
+                        self.field.setAttribute("type", "text");
+                    } else {
+                        self.field.setAttribute("type", "password");
+                    }
+                }, false);
+            }
+        };
+
+        $(".alert")
+            .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+                function (e) {
+                    $(this).css("display", "none");
+                });
+    });
+
+function setFormValidation(formId) {
+
+    var form = $(`#${formId}`),
+        formData = $.data(form[0]),
+        settings = formData.validator.settings,
+        oldErrorPlacement = settings.errorPlacement,
+        oldSuccess = settings.success;
+
+    settings.errorPlacement = function (label, element) {
+        oldErrorPlacement(label, element);
+
+        label.parents(".form-group").addClass("has-error");
+        label.addClass("text-danger");
+    };
+
+    settings.success = function (label) {
+        label.parents(".form-group").removeClass("has-error");
+
+        oldSuccess(label);
+    };
+}
