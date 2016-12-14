dd5c9cbfc3c2680a81b0dcfb2b9c47a4b7f38539
iframeworx <geert@iframeworx.be>
2016-12-14 14:19:07 +0100

    add custom date validation attributes

---
+++ BootstrapMvc.UI/ViewModels/Validation/AtLeastTodayAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BootstrapMvc.UI.ViewModels.Validation
+{
+    public class AtLeastTodayAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+
+            if (DateTime.TryParse(value.ToString(), out date))
+                return date >= DateTime.Now.Date;
+
+            return false;
+        }
+    }
+}
---
+++ BootstrapMvc.UI/ViewModels/Validation/MinimumAgeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BootstrapMvc.UI.ViewModels.Validation
+{
+    public class MinimumAgeAttribute : ValidationAttribute
+    {
+        private readonly int _minimumAge;
+
+        public MinimumAgeAttribute(int minimumAge)
+        {
+            _minimumAge = minimumAge;
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+
+            if (DateTime.TryParse(value.ToString(), out date))
+                return date.AddYears(_minimumAge) < DateTime.Now;
+
+            return false;
+        }
+    }
+}
