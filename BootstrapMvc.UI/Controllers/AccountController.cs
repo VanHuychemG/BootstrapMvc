9fe87e1bff51b79dccb61fd2664b8f0fde886e2b
VanHuychemG <geert@exira.com>
2017-01-16 20:57:53 +0100

    add login form w/identity

--- BootstrapMvc.UI/Controllers/AccountController.cs
+++ BootstrapMvc.UI/Controllers/AccountController.cs
@@ -1,13 +1,31 @@
-﻿using BootstrapMvc.Core.Controllers;
+﻿using System.Threading.Tasks;
+
+using BootstrapMvc.Core.Controllers;
+using BootstrapMvc.UI.Infrastructure;
+using BootstrapMvc.UI.ViewModels;
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BootstrapMvc.Core.Extensions;
 
 namespace BootstrapMvc.UI.Controllers
 {
     [Authorize]
     public class AccountController : BaseController
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public AccountController(
+            SignInManager<ApplicationUser> signInManager,
+            ILoggerFactory loggerFactory)
+        {
+            _signInManager = signInManager;
+            _logger = loggerFactory.CreateLogger<AccountController>();
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -22,16 +40,54 @@ namespace BootstrapMvc.UI.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Login(LoginViewModel model, string returnUrl = null)
-        //{
-        //    ViewData["ReturnUrl"] = returnUrl;
-        //    if (!ModelState.IsValid)
-        //        return View(model);
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (!ModelState.IsValid)
+                return View(model);
 
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation(1, "User logged in.");
 
-        //}
+                return RedirectToLocal(returnUrl);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning(2, "User account locked out.");
+
+                return View("Lockout");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid username and/or password.");
+
+            Danger("Looks like something went wrong. Please check your form.", ModelState.ToErrorMessages());
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            _logger.LogInformation(4, "User logged out.");
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
     }
 }
--- BootstrapMvc.UI/ViewModels/LoginViewModel.cs
+++ BootstrapMvc.UI/ViewModels/LoginViewModel.cs
@@ -5,11 +5,15 @@ namespace BootstrapMvc.UI.ViewModels
     public class LoginViewModel
     {
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
